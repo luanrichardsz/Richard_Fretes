75802dc36c4018979707e3ce8b40d05b4c5e7380
luanrichardsz <luanrchardof@gmail.com>
2026-04-24 14:17:50 -0300

    fetch(16): Cria o servlet de Menu para fazer a chamada do menu

---
+++ src/main/java/br/com/controller/MenuServlet.java
@@ -0,0 +1,17 @@
+package br.com.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/menu")
+public class MenuServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/jsp/menu/menu.jsp").forward(req, resp);
+    }
+}
