5db9ba39096c218bb5acbf65cc5fa3603fd09b1e
luanrichardsz <luanrchardof@gmail.com>
2026-05-03 23:26:51 -0300

    style(52): Configura o Servlet de Menu para retornar os dados da empresa na tela principal

--- src/main/java/br/com/menu/MenuServlet.java
+++ src/main/java/br/com/menu/MenuServlet.java
@@ -1,6 +1,10 @@
 package br.com.menu;
 
+import br.com.cliente.ClienteDAO;
+import br.com.frete.FreteDAO;
+import br.com.motorista.MotoristaDAO;
 import br.com.usuario.Usuario;
+import br.com.veiculo.VeiculoDAO;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,9 +12,15 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet("/menu")
 public class MenuServlet extends HttpServlet {
+    private final FreteDAO freteDAO = new FreteDAO();
+    private final MotoristaDAO motoristaDAO = new MotoristaDAO();
+    private final VeiculoDAO veiculoDAO = new VeiculoDAO();
+    private final ClienteDAO clienteDAO = new ClienteDAO();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
@@ -19,6 +29,24 @@ public class MenuServlet extends HttpServlet {
             resp.sendRedirect("login");
             return;
         }
+
+        if (usuario.isAdmin()) {
+            req.setAttribute("fretes", freteDAO.listarTodos());
+            req.setAttribute("motoristas", motoristaDAO.listarTodos());
+            req.setAttribute("veiculos", veiculoDAO.listarTodos());
+            req.setAttribute("clientes", clienteDAO.listarTodos());
+        } else if (usuario.getClienteId() != null) {
+            req.setAttribute("fretes", freteDAO.listarPorCliente(usuario.getClienteId()));
+            req.setAttribute("motoristas", motoristaDAO.listarPorCliente(usuario.getClienteId()));
+            req.setAttribute("veiculos", veiculoDAO.listarPorCliente(usuario.getClienteId()));
+            req.setAttribute("clientes", new ArrayList<>());
+        } else {
+            req.setAttribute("fretes", new ArrayList<>());
+            req.setAttribute("motoristas", new ArrayList<>());
+            req.setAttribute("veiculos", new ArrayList<>());
+            req.setAttribute("clientes", new ArrayList<>());
+        }
+
         req.getRequestDispatcher("/WEB-INF/jsp/menu/menu.jsp").forward(req, resp);
     }
 }
